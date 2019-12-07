209ba2b4488697e09bfde22581900dbca6976040
infected521 <30352728+infected521@users.noreply.github.com>
2019-12-06 21:38:01 -0600

    Add files via upload

--- request/ssl.sh
+++ request/ssl.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-fun_trans () { 
+
+SCPdir="/etc/newadm"
+SCPusr="${SCPdir}/ger-user"
+SCPfrm="/etc/ger-frm"
+SCPfrm3="/etc/adm-lite"
+SCPinst="/etc/ger-inst"
+SCPidioma="${SCPdir}/idioma"
+
+
+declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;31m" [3]="\033[1;33m" [4]="\033[1;32m" )
+SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
+SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
+API_TRANS="aHR0cHM6Ly93d3cuZHJvcGJveC5jb20vcy9vc29jdzVrNm1zc2xncGEvdHJhbnMK"
+SUB_DOM='base64 -d'
+fun_trans ${id}  () { 
 local texto
 local retorno
 declare -A texto
+[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans $(echo $API_TRANS|$SUB_DOM) &> /dev/null
 [[ ! -e /etc/texto-adm ]] && touch /etc/texto-adm
 source /etc/texto-adm
 if [[ -z $(echo ${texto[$2]}) ]]; then
 ENGINES=(aspell google deepl bing spell hunspell apertium yandex)
   while [[ -z $retorno ]]; do
     NUM="$(($RANDOM%${#ENGINES[@]}))"
-    retorno="$(source trans -e ${ENGINES[$NUM]} -b pt:$1 "$2"|sed -e 's/[^a-z0-9 -]//ig' 2>/dev/null)"
+    retorno="$(source trans -e ${ENGINES[$NUM]} -b es:$1 "$2"|sed -e 's/[^a-z0-9 -]//ig' 2>/dev/null)"
   done
  echo "texto[$2]='$retorno'"  >> /etc/texto-adm
  echo "$retorno"
@@ -17,14 +32,7 @@ else
  echo "${texto[$2]}"
 fi
 }
-declare -A cor=( [0]="\033[33m" [1]="\033[33m" [2]="\033[33m" [3]="\033[33m" [4]="\033[33m" )
-barra="\e[33m======================================================\033[1;37m"
-[[ -z $1 ]] && exit || id=$1
-SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
-SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
-API_TRANS="aHR0cHM6Ly93d3cuZHJvcGJveC5jb20vcy9sNmlxZjV4anRqbXBkeDUvdHJhbnM/ZGw9MA=="
-SUB_DOM='base64 -d'
-wget -O /usr/bin/trans $(echo $API_TRANS|$SUB_DOM) &> /dev/null
+
 mportas () {
 unset portas
 portas_var=$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LISTEN")
@@ -35,101 +43,128 @@ done <<< "$portas_var"
 i=1
 echo -e "$portas"
 }
-fun_ip () {
-MEU_IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-MEU_IP2=$(wget -qO- ipv4.icanhazip.com)
-[[ "$MEU_IP" != "$MEU_IP2" ]] && IP="$MEU_IP2" || IP="$MEU_IP"
-}
-fun_eth () {
-eth=$(ifconfig | grep -v inet6 | grep -v lo | grep -v 127.0.0.1 | grep "encap:Ethernet" | awk '{print $1}')
-    [[ $eth != "" ]] && {
-    echo -e "$barra"
-    echo -e "${cor[3]} $(fun_trans ${id} "Aplicar Sistema Para Melhorar Pacotes Ssh?")"
-    echo -e "${cor[3]} $(fun_trans ${id} "Opcao Para Usuarios Avancados")"
-    echo -e "$barra"
-    read -p " [S/N]: " -e -i n sshsn
-           [[ "$sshsn" = @(s|S|y|Y) ]] && {
-           echo -e "${cor[1]} $(fun_trans ${id} "Correcao de problemas de pacotes no SSH...")"
-           echo -e " $(fun_trans ${id} "Qual A Taxa RX")"
-           echo -ne "[ 1 - 999999999 ]: "; read rx
-           [[ "$rx" = "" ]] && rx="999999999"
-           echo -e " $(fun_trans ${id} "Qual A Taxa TX")"
-           echo -ne "[ 1 - 999999999 ]: "; read tx
-           [[ "$tx" = "" ]] && tx="999999999"
-           apt-get install ethtool -y > /dev/null 2>&1
-           ethtool -G $eth rx $rx tx $tx > /dev/null 2>&1
-           }
-     echo -e "$barra"
-     }
-}
-fun_bar () {
-comando="$1"
- _=$(
-$comando > /dev/null 2>&1
-) & > /dev/null
-pid=$!
-while [[ -d /proc/$pid ]]; do
-echo -ne " \033[33m["
-   for((i=0; i<10; i++)); do
-   echo -ne "\033[33m##"
-   sleep 0.2
-   done
-echo -ne "\033[33m]"
-sleep 1s
-echo
-tput cuu1
-tput dl1
-done
-echo -e " \033[1;33m[\033[33m####################\033[1;33m] - \033[1;32m100%\033[0m"
-sleep 1s
-}
 ssl_stunel () {
 [[ $(mportas|grep stunnel4|head -1) ]] && {
-echo -e "\033[1;33m $(fun_trans ${id} "Parando Stunnel")"
-echo -e "$barra"
+echo -e "\033[1;33m $(fun_trans ${id}  ${id}   "Parando Stunnel")"
+msg -bar
 fun_bar "apt-get purge stunnel4 -y"
-echo -e "$barra"
-echo -e "\033[1;33m $(fun_trans ${id} "Parado Com Sucesso!")"
-echo -e "$barra"
+msg -bar
+echo -e "\033[1;33m $(fun_trans ${id}   "Parado Con Exito!")"
+msg -bar
 return 0
 }
-echo -e "\033[33m $(fun_trans ${id} "SSL GOLDEN ADM PRO")"
-echo -e "$barra"
-echo -e "\033[1;33m $(fun_trans ${id} "Selecione Uma Porta De Redirecionamento Interna")"
-echo -e "\033[1;33m $(fun_trans ${id} "Ou seja, uma Porta no Seu Servidor Para o SSL")"
-echo -e "$barra"
+echo -e "\033[1;32m $(fun_trans ${id}   "INSTALADOR SSL LA CASITA DEL TERROR")"
+msg -bar
+echo -e "\033[1;33m $(fun_trans ${id}   "Seleccione una puerta de redirección interna.")"
+echo -e "\033[1;33m $(fun_trans ${id}   "Es decir, un puerto en su servidor para SSL")"
+msg -bar
          while true; do
          echo -ne "\033[1;37m"
          read -p " Local-Port: " portx
          if [[ ! -z $portx ]]; then
              if [[ $(echo $portx|grep [0-9]) ]]; then
-                [[ $(mportas|grep $portx|head -1) ]] && break || echo -e "\033[1;31m $(fun_trans ${id} "Porta Invalida")"
+                [[ $(mportas|grep $portx|head -1) ]] && break || echo -e "\033[1;31m $(fun_trans ${id}   "Puerta invalida")"
              fi
          fi
          done
-echo -e "$barra"
+msg -bar
 DPORT="$(mportas|grep $portx|awk '{print $2}'|head -1)"
-echo -e "\033[1;33m $(fun_trans ${id} "Agora Presizamos Saber Qual Porta o SSL, Vai Escutar")"
-echo -e "$barra"
+echo -e "\033[1;33m $(fun_trans ${id}   "Ahora Prestamos Saber Que Puerta del SSL, Va a Escuchar")"
+msg -bar
     while true; do
     read -p " Listen-SSL: " SSLPORT
     [[ $(mportas|grep -w "$SSLPORT") ]] || break
-    echo -e "\033[1;33m $(fun_trans ${id} "esta Porta Ja esta em Uso")"
+    echo -e "\033[1;33m $(fun_trans ${id}   "Esta puerta está en uso")"
     unset SSLPORT
     done
-echo -e "$barra"
-echo -e "\033[33m $(fun_trans ${id} "Instalando SSL")"
-echo -e "$barra"
+msg -bar
+echo -e "\033[1;33m $(fun_trans ${id}   "Instalando SSL")"
+msg -bar
 fun_bar "apt-get install stunnel4 -y"
-echo -e "cert = /etc/stunnel/stunnel.pem\nclient = no\nsocket = a:SO_REUSEADDR=1\nsocket = l:TCP_NODELAY=1\nsocket = r:TCP_NODELAY=1\n\n[stunnel]\nconnect = 127.0.0.1:${DPORT}\naccept = ${SSLPORT}" > /etc/stunnel/stunnel.conf
-openssl genrsa -out key.pem 2048 > /dev/null 2>&1
-(echo DS; echo @DEADSSHOT593; echo VENTA DE VPS DE PAGA; echo WHATSAPP +593996534252; echo TELEGRAM @DEADSHOT593; echo @DEADSHOT593; echo @aDEADSHOT593)|openssl req -new -x509 -key key.pem -out cert.pem -days 1095 > /dev/null 2>&1
-cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+echo -e "client = no\n[SSL]\ncert = /etc/stunnel/stunnel.pem\naccept = ${SSLPORT}\nconnect = 127.0.0.1:${DPORT}" > /etc/stunnel/stunnel.conf
+####Coreccion2.0##### 
+openssl genrsa -out stunnel.key 2048 > /dev/null 2>&1
+
+(echo "La" ; echo "Ca" ; echo "si" ; echo "ta" ; echo "del" ; echo "te" ; echo "rror" )|openssl req -new -key stunnel.key -x509 -days 1000 -out stunnel.crt > /dev/null 2>&1
+
+cat stunnel.crt stunnel.key > stunnel.pem 
+
+mv stunnel.pem /etc/stunnel/
+######-------
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 service stunnel4 restart > /dev/null 2>&1
-echo -e "$barra"
-echo -e "\033[1;33m $(fun_trans ${id} "INSTALADO COM SUCESSO")"
-echo -e "$barra"
+msg -bar
+echo -e "\033[1;33m $(fun_trans ${id}   "INSTALADO CON EXITO")"
+msg -bar
+rm -rf /etc/ger-frm/stunnel.crt > /dev/null 2>&1
+rm -rf /etc/ger-frm/stunnel.key > /dev/null 2>&1
+rm -rf /root/stunnel.crt > /dev/null 2>&1
+rm -rf /root/stunnel.key > /dev/null 2>&1
 return 0
 }
+ssl_stunel_2 () {
+echo -e "\033[1;32m $(fun_trans ${id}   "INSTALADOR SSL LA CASITA DEL TERROR")"
+msg -bar
+echo -e "\033[1;33m $(fun_trans ${id}   "Seleccione una puerta de redirección interna.")"
+echo -e "\033[1;33m $(fun_trans ${id}   "Es decir, un puerto en su servidor para SSL")"
+msg -bar
+         while true; do
+         echo -ne "\033[1;37m"
+         read -p " Local-Port: " portx
+         if [[ ! -z $portx ]]; then
+             if [[ $(echo $portx|grep [0-9]) ]]; then
+                [[ $(mportas|grep $portx|head -1) ]] && break || echo -e "\033[1;31m $(fun_trans ${id}   "Puerta invalida")"
+             fi
+         fi
+         done
+msg -bar
+DPORT="$(mportas|grep $portx|awk '{print $2}'|head -1)"
+echo -e "\033[1;33m $(fun_trans ${id}   "Ahora Escribiremos Que Puerta del SSL, Va a Escuchar")"
+msg -bar
+    while true; do
+    read -p " Listen-SSL: " SSLPORT
+    [[ $(mportas|grep -w "$SSLPORT") ]] || break
+    echo -e "\033[1;33m $(fun_trans ${id}   "Esta puerta está en uso")"
+    unset SSLPORT
+    done
+msg -bar
+echo -e "\033[1;33m $(fun_trans ${id}   "Instalando SSL")"
+msg -bar
+fun_bar "apt-get install stunnel4 -y"
+echo -e "client = no\n[SSL+]\ncert = /etc/stunnel/stunnel.pem\naccept = ${SSLPORT}\nconnect = 127.0.0.1:${DPORT}" >> /etc/stunnel/stunnel.conf
+######-------
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+service stunnel4 restart > /dev/null 2>&1
+msg -bar
+echo -e "\033[1;33m $(fun_trans ${id}   "INSTALADO CON EXITO")"
+msg -bar
+rm -rf /etc/ger-frm/stunnel.crt > /dev/null 2>&1
+rm -rf /etc/ger-frm/stunnel.key > /dev/null 2>&1
+rm -rf /root/stunnel.crt > /dev/null 2>&1
+rm -rf /root/stunnel.key > /dev/null 2>&1
+return 0
+}
+echo -e "${cor[3]} $(fun_trans ${id} "INSTALADOR SSL LA CASITA DEL TERROR")"
+msg -bar
+echo -e "${cor[1]} $(fun_trans ${id} "Escoja la opcion deseada.")"
+msg -bar
+echo "1).- $(fun_trans ${id} "ININICIAR O PARAR SSL")"
+echo "2).- $(fun_trans ${id} "AGREGAR PUERTOS SSL")"
+msg -bar
+echo -n "$(fun_trans ${id} "Digite solo el numero segun su respuesta: ")"
+read opcao
+case $opcao in
+1)
+msg -bar
 ssl_stunel
+;;
+2)
+msg -bar
+echo -e "\033[1;93m  $(fun_trans ${id} "AGREGAR SSL EXTRA  ...")"
+msg -bar
+ssl_stunel_2
+sleep 3
+exit
+;;
+esac
+
